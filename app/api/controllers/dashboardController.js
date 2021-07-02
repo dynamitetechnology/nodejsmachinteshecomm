3ffaa12c37d0d8814de990f3ae11e354399ec7f6
dynamitetechnology <technologydynamite@gmail.com>
2021-07-03 01:18:22 +0530

    03-07-2021

--- app/api/controllers/dashboardController.js
+++ app/api/controllers/dashboardController.js
@@ -15,103 +15,12 @@ module.exports = {
 
             let payload = jwt.verify(token, process.env.JWT_SECRET)
             console.log('payload-------->', payload)
-            res.render("dashboard.ejs")
-        })();
-    },
 
-    addproduct:(req, res, next)=>{
-        
-        ;(async () => {
-            let db = req.app.locals.db;
-            const token = req.cookies.token;
-            let errors = validationResult(req);
 
-            let payload = jwt.verify(token, process.env.JWT_SECRET)
-            console.log('payload-------->', payload)
-            res.render("addproducts.ejs")
-        })();
-    },
-
-    getuserdetail: (req, res, next) => {
-
-        ;(async () => {
-            let db = req.app.locals.db;
-            const token = req.cookies.token;
-            let errors = validationResult(req);
-
-            let payload = jwt.verify(token, process.env.JWT_SECRET)
-            console.log('payload-------->', payload)
-
-            // if (req.body.email == payload.username) {
-                let userinfo = await new Promise(function (resolve) {
-                    const stmt = {
-                        text: `select  name, email, phone, password, image from  users where email = $1`,
-                        values: [req.body.email]
-                    }
-                    db.query(stmt, async function (err, result) {
-                        if (err) throw err;
-                        let user = await result.rows[0];
-                        return resolve(user)
-                    })
-                });
-
-                res.json({
-                    status: 200,
-                    message: "success",
-                    userinfo: userinfo
-                })
-            // } else {
-            //     res.json({
-            //         status: 200,
-            //         message: "Invalid Token"
-            //     })
-            // }
-        })()
-    },
-
-    addpost: (req, res, next) => {
-
-        ;(async () => {
-            let db = req.app.locals.db;
-            const token = req.cookies.token;
-            let errors = validationResult(req);
-
-            let payload = jwt.verify(token, process.env.JWT_SECRET)
-
-            let userinfo = await new Promise(function (resolve) {
+            let productList = await new Promise(function (resolve) {
                 const stmt = {
-                    text: `insert into post_collection (title, description, createdby) values ($1, $2, $3)`,
-                    values: [req.body.title, req.body.description, payload.username]
-                }
-                db.query(stmt, async function (err, result) {
-                    if (err) throw err;
-                    let user = await result.rows[0];
-                    return resolve(user)
-                })
-            });
-
-            res.json({
-                status: 200,
-                message: "successfully posted"
-            })
-
-        })()
-    },
-
-
-    userpostlist: (req, res, next) => {
-
-        ;(async () => {
-            let db = req.app.locals.db;
-            const token = req.cookies.token;
-            let errors = validationResult(req);
-
-            let payload = jwt.verify(token, process.env.JWT_SECRET)
-
-            let userpost = await new Promise(function (resolve) {
-                const stmt = {
-                    text: `select  title, description, createdby from post_collection where createdby = $1`,
-                    values: [req.body.username]
+                    text: `SELECT id, product_name, product_price, stock, product_description, product_image FROM products order by id desc`,
+                    values: []
                 }
                 db.query(stmt, async function (err, result) {
                     if (err) throw err;
@@ -119,12 +28,44 @@ module.exports = {
                     return resolve(user)
                 })
             });
+console.log(productList)
+            res.render("dashboard.ejs",{productList:productList})
+        })();
+    },
 
-            res.json({
-                status: 200,
-                message: "success",
-                userpost: userpost
-            })
-        })()
+    addproduct:(req, res, next)=>{
+        ;(async () => {
+            let db = req.app.locals.db;
+            const token = req.cookies.token;
+            let errors = validationResult(req);
+            let payload = jwt.verify(token, process.env.JWT_SECRET)
+            console.log('payload-------->', payload)
+            res.render("addproducts.ejs")
+        })();
+    },
+
+    deleteproduct: (req,res,next) => {
+        ;(async () => {
+            let db = req.app.locals.db;
+            const token = req.cookies.token;
+            let errors = validationResult(req);
+console.log("Hello Print")
+            let productList = await new Promise(function (resolve) {
+                const stmt = {
+                    text: `delete FROM products where id = $1`,
+                    values: [req.body.prid]
+                }
+                db.query(stmt, async function (err, result) {
+                    if (err) throw err;
+                    let user = await result.rows[0];
+                    return resolve(user)
+                })
+            });
+        console.log(productList)
+            res.json({"status":"200"});
+
+        })();
     }
+
+    
 }
--- app/api/routers/dashboardRouter.js
+++ app/api/routers/dashboardRouter.js
@@ -1,19 +1,68 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/dashboardController');
-
 const { check, validationResult} = require('express-validator');
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1000000
+    }
+})
 
 router.get('/dashboard', controller.getDashboard);
 
 router.get('/addproduct', controller.addproduct)
 
-router.post('/getuserdetail', controller.getuserdetail);
+router.post('/addproduct', [
+        check('product_name', 'Name Must Not Be Eempty').exists().notEmpty().trim().escape(),
+        check('product_price', 'Price Must Not Be Empty.').exists().notEmpty().trim().escape(),
+        check('product_stock', 'Stock Must Not Be Empty.').exists().notEmpty().trim().isEmail().normalizeEmail().escape(),
+        check('product_disc', 'Desc Must Not Be Empty.').exists().notEmpty().trim().isLength({
+            min: 6
+        }).escape()
+    ],
+    upload.single('product_image'), (req, res, next) => {
 
-router.post('/postadd', controller.addpost);
+        let db = req.app.locals.db;
+        let loginToken = req.cookies.token;
+        let errors = validationResult(req);
+  
 
-router.post('/userpostlist',controller.userpostlist)
+        (async () => {
 
-//router.get('/dashboard',controller.dashboard)
+            console.log('Res----------->',req.file.path)
+
+            let addnewProducts = await new Promise(function (resolve) {
+                const stmt = {
+                    text: `insert into products (product_name, product_price, stock, product_description, product_image) values($1, $2, $3, $4, $5)`,
+                    values: [req.body.product_name, req.body.product_price, req.body.product_stock, req.body.product_disc, req.file.path]
+                }
+                db.query(stmt, async function (err, result) {
+                    if (err) throw err;
+                    let user = await result.rows[0];
+                    return resolve(user)
+                })
+            });
+
+            res.redirect("/dashboard")
+            // res.json({
+            //     "status": 200,
+            //     "message": "user create successfully"
+            // })
+
+        })()
+    });
+
+router.post('/deleteproduct',controller.deleteproduct)
 
 module.exports = router;
